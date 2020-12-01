2f535af373bb93e615a2548e66881b353e7ecf03
Max Miller <maxmiller413@gmail.com>
2020-12-01 04:47:41 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -11,4 +11,5 @@ def square_array(numbers)
   new_array = []
   counter = 0 
   while counter < numbers.length
-    new_array.push(numbers[counter])
+    new_array.push(numbers[counter]) ** 2 
+    
